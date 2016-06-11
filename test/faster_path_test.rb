4ce0352edc27f4b17e69c5dd0e42188a0c541d47
Daniel P. Clark <6ftdan@gmail.com>
2016-06-11 15:13:44 -0400

    jRuby over 9000

---
+++ test/faster_path_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class FasterPathTest < Minitest::Test
+  def test_it_build_linked_library
+    assert File.exist? begin
+      prefix = Gem.win_platform? ? "" : "lib"
+      "#{File.expand_path("../target/release/", File.dirname(__FILE__))}/#{prefix}faster_path.#{FFI::Platform::LIBSUFFIX}"
+    end
+  end
+end
