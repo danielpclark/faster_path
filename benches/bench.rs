ee9eaef943fc81f809d7dea5f653346fffb358f3
Daniel P. Clark <6ftdan@gmail.com>
2016-06-13 02:39:09 -0400

    Benchmarks & sanctioned Rust code for Ruby Strings
    [ci skip]

---
+++ benches/bench.rs
@@ -0,0 +1,65 @@
+#![feature(test)]
+// PLAYGROUND FOR PERFORMANCE TESTS
+//
+extern crate libc;
+extern crate test;
+use test::Bencher;
+
+use libc::c_char;
+use std::ffi::{CStr,CString};
+use std::str;
+
+#[bench]
+fn rust_to_ruby_c_char_convenient(b: &mut Bencher){
+  let s: String = "hello".to_string();
+
+  // (185ns)
+  b.iter(|| { 
+    let s = s.clone(); //To imitate .into()
+    let s_slice: &str = &s[..];
+    CString::new(s_slice).unwrap().into_raw()
+  })
+}
+
+// SANCTIONED USAGE
+#[bench]
+fn rust_to_ruby_to_c_char(b: &mut Bencher){
+  let s: String = "hello".to_string();
+  let s = s.as_str();
+
+  // USE THIS METHOD!!! (153ns)
+  b.iter(|| { 
+    CString::new(s).unwrap().into_raw()
+  })
+}
+
+
+#[bench]
+fn ruby_to_rust_string_convenient(b: &mut Bencher){
+  let s: *const c_char = CString::new("hello").unwrap().into_raw();
+
+  // (46ns)
+  b.iter(|| { 
+    let c_str = unsafe {
+      assert!(!s.is_null());
+      CStr::from_ptr(s)
+    };
+    (*str::from_utf8(c_str.to_bytes()).unwrap_or("")).to_string()
+  })
+}
+
+
+// SANCTIONED USAGE
+#[bench]
+fn ruby_to_rust_from_c_char(b: &mut Bencher){
+  let s: *const c_char = CString::new("hello").unwrap().into_raw();
+
+  // USE THIS METHOD!!! (19ns)
+  b.iter(|| { 
+    let c_str = unsafe {
+      assert!(!s.is_null());
+      CStr::from_ptr(s)
+    };
+    str::from_utf8(c_str.to_bytes()).unwrap_or("")
+  })
+}
--- src/ruby_string.rs
+++ src/ruby_string.rs
@@ -5,9 +5,11 @@ pub struct RubyString;
 // to write all of this code out in each method using just str.
 //
 // Using these methods you will still get you 10X (= 900%) performance
-// gain regardless.  But really consider not using String.  If you do,
-// the performance will go from 900% to 1250% gains.
+// gain regardless.  But really consider not using String.  If you do
+// as I've intstructed the performance gains will go from 900% to 1250%.
 impl RubyString {
+  // FOR QUICK IMPLEMENTATION.  NOT FOR PRODUCTION.
+  // SEE BENCHMARKS FOR SANCTIONED IMPLEMENTATION.
   fn from_ruby(s: *const c_char) -> String {
     let c_str = unsafe {
       assert!(!s.is_null());
@@ -16,6 +18,8 @@ impl RubyString {
     (*str::from_utf8(c_str.to_bytes()).unwrap_or("")).to_string()
   }
 
+  // FOR QUICK IMPLEMENTATION.  NOT FOR PRODUCTION.
+  // SEE BENCHMARKS FOR SANCTIONED IMPLEMENTATION.
   fn to_ruby<S: Into<String>>(s: S) -> *const c_char {
     let r_str = s.into();
     let s_slice: &str = &r_str[..];
