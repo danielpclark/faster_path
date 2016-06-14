ffa8ef7cd6e89f62d5c37ba6d0d1e50ad4100f57
Daniel P. Clark <6ftdan@gmail.com>
2016-06-14 00:44:35 -0400

    Ripple effect test for Pathname#join

---
+++ test/ripple_effects/join_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'faster_path/optional/refinements'
+
+class RefinedPathname
+  using FasterPath::RefinePathname
+  def join(*args)
+    a, *rest = args
+    Pathname(a).join(*rest)
+  end 
+end
+
+class JoinTest < Minitest::Test
+  def test_join
+    r = RefinedPathname.new.join("a",Pathname("b"), Pathname("c"))
+    assert_equal(Pathname("a/b/c"), r)
+    r = RefinedPathname.new.join("/a",Pathname("b"), Pathname("c"))
+    assert_equal(Pathname("/a/b/c"), r)
+    r = RefinedPathname.new.join("/a",Pathname("/b"), Pathname("c"))
+    assert_equal(Pathname("/b/c"), r)
+    r = RefinedPathname.new.join("/a",Pathname("/b"), Pathname("/c"))
+    assert_equal(Pathname("/c"), r)
+    r = RefinedPathname.new.join("/a","/b", "/c")
+    assert_equal(Pathname("/c"), r)
+    r = RefinedPathname.new.join("/foo/var")
+    assert_equal(Pathname("/foo/var"), r)
+  end
+end
