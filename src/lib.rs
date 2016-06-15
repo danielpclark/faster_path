b1ed9170023261f53d8a93dcd31486bb4ea21855
Daniel P. Clark <6ftdan@gmail.com>
2016-06-14 21:12:23 -0400

    regex helper for method

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,34 @@
-// Copyright 2015-2016 Daniel P. Clark & Other Combinatorics Developers
+// Copyright 2015-2016 Daniel P. Clark & Other FasterPath Developers
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 extern crate libc;
+extern crate regex;
 
 use std::path::{Path,MAIN_SEPARATOR};
 use libc::c_char;
 use std::ffi::{CStr,CString,OsStr};
 use std::str;
 use std::mem;
+
+fn pathish_chomp_regex(string: &str, globish_string: &str) -> String {
+  string.to_string()
+}
+
+#[test]
+fn it_chomps_strings_correctly(){
+  assert_eq!(pathish_chomp_regex("",""), "");
+  assert_eq!(pathish_chomp_regex("ruby",""), "ruby");
+  assert_eq!(pathish_chomp_regex("ruby.rb",".rb"), "ruby");
+  assert_eq!(pathish_chomp_regex("ruby.rb",".*"), "ruby");
+  assert_eq!(pathish_chomp_regex(".ruby/ruby.rb",".rb"), ".ruby/ruby");
+  assert_eq!(pathish_chomp_regex(".ruby/ruby.rb.swp",".rb"), ".ruby/ruby.rb.swp");
+  assert_eq!(pathish_chomp_regex(".ruby/ruby.rb.swp",".swp"), ".ruby/ruby.rb");
+  assert_eq!(pathish_chomp_regex(".ruby/ruby.rb.swp",".*"), ".ruby/ruby.rb");
+}
+
 
 include!("ruby_string.rs");
 include!("ruby_array.rs");
