c2662516ccf49181ff2cc6a15bd1c497203e7b5f
Daniel P. Clark <6ftdan@gmail.com>
2016-06-15 20:32:47 -0400

    More ripple effect tests

---
+++ test/ripple_effects/del_trailing_separator_test.rb
@@ -0,0 +1,54 @@
+require 'test_helper'
+require 'faster_path/optional/refinements'
+
+class RefinedPathname
+  using FasterPath::RefinePathname
+  def del_trailing_separator(pth)
+    Pathname.allocate.send(:del_trailing_separator, pth)
+  end 
+end
+
+class DelTrailingSeparatorTest < Minitest::Test
+  def test_del_trailing_separator
+    assert_equal RefinedPathname.new.del_trailing_separator("/")   ,"/"
+    assert_equal RefinedPathname.new.del_trailing_separator("/a")  ,"/a"
+    assert_equal RefinedPathname.new.del_trailing_separator("/a/") ,"/a"
+    assert_equal RefinedPathname.new.del_trailing_separator("/a//"),"/a"
+    assert_equal RefinedPathname.new.del_trailing_separator(".")   ,"." 
+    assert_equal RefinedPathname.new.del_trailing_separator("./")  ,"." 
+    assert_equal RefinedPathname.new.del_trailing_separator(".//") ,"." 
+  end
+
+  def test_del_trailing_separator_in_dosish_context
+    if File.dirname("A:") == "A:." #DOSISH_DRIVE_LETTER
+      assert_equal RefinedPathname.new.del_trailing_separator("A:")   ,"A:"
+      assert_equal RefinedPathname.new.del_trailing_separator("A:/")  ,"A:/"
+      assert_equal RefinedPathname.new.del_trailing_separator("A://") ,"A:/"
+      assert_equal RefinedPathname.new.del_trailing_separator("A:.")  ,"A:."
+      assert_equal RefinedPathname.new.del_trailing_separator("A:./") ,"A:."
+      assert_equal RefinedPathname.new.del_trailing_separator("A:.//"),"A:."
+    end
+
+    if File.dirname("//") == "//" # DOSISH_UNC
+      assert_equal RefinedPathname.new.del_trailing_separator("//")       ,"//"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a")      ,"//a"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a/")     ,"//a"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a//")    ,"//a"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a/b")    ,"//a/b"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a/b/")   ,"//a/b"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a/b//")  ,"//a/b"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a/b/c")  ,"//a/b/c"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a/b/c/") ,"//a/b/c"
+      assert_equal RefinedPathname.new.del_trailing_separator("//a/b/c//"),"//a/b/c"
+    else
+      assert_equal RefinedPathname.new.del_trailing_separator("///")  ,"/"
+      assert_equal RefinedPathname.new.del_trailing_separator("///a/"),"///a"
+    end
+
+    if File::ALT_SEPARATOR != nil # DOSISH
+      assert_equal RefinedPathname.new.del_trailing_separator("a\\"),"a"
+      assert_equal RefinedPathname.new.del_trailing_separator("\225\\\\".dup.force_encoding("cp932")),"\225\\".dup.force_encoding("cp932")
+      assert_equal RefinedPathname.new.del_trailing_separator("\225\\\\".dup.force_encoding("cp437")),"\225".dup.force_encoding("cp437")
+    end
+  end
+end
---
+++ test/ripple_effects/has_trailing_separator_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+require 'faster_path/optional/refinements'
+
+class RefinedPathname
+  using FasterPath::RefinePathname
+  def has_trailing_separator?(pth)
+    Pathname.allocate.send(:has_trailing_separator?, pth)
+  end 
+end
+
+class HasTrailingSeparatorTest < Minitest::Test
+  def test_has_trailing_separator?
+    refute RefinedPathname.new.has_trailing_separator?("/")
+    refute RefinedPathname.new.has_trailing_separator?("///")
+    refute RefinedPathname.new.has_trailing_separator?("a")
+    assert RefinedPathname.new.has_trailing_separator?("a/")
+  end
+end
