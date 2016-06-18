132b1a23f9db1682dbd7c439d65d99a1eda8f055
Matt Blewitt <blewitt.matt@gmail.com>
2016-06-18 07:59:55 +0100

    Updates from feedback
    * Change README to reflect actual method type
    * Add tests to compare FasterPath implementation
      against Pathname implementation

--- test/add_trailing_separator_test.rb
+++ test/add_trailing_separator_test.rb
@@ -24,4 +24,54 @@ class AddTrailingSeparatorTest < Minitest::Test
       assert_equal FasterPath.add_trailing_separator('A:.//'), 'A:.//'
     end
   end
+
+  def test_add_trailing_separator_against_pathname_implementation
+    result_pair = -> (str) {
+      [
+        Pathname.allocate.send(:add_trailing_separator, str),
+        FasterPath.add_trailing_separator(str)
+      ]
+    }
+
+    assert_equal(*result_pair.(''))
+    assert_equal(*result_pair.('/'))
+    assert_equal(*result_pair.('//'))
+    assert_equal(*result_pair.('hello'))
+    assert_equal(*result_pair.('hello/'))
+    assert_equal(*result_pair.('/hello/'))
+    assert_equal(*result_pair.('/hello//'))
+    assert_equal(*result_pair.('.'))
+    assert_equal(*result_pair.('./'))
+    assert_equal(*result_pair.('.//'))
+    assert_equal(*result_pair.("aa/a//a"))
+    assert_equal(*result_pair.("/aaa/a//a"))
+    assert_equal(*result_pair.("/aaa/a//a/a"))
+    assert_equal(*result_pair.("/aaa/a//a/a"))
+    assert_equal(*result_pair.("a//aaa/a//a/a"))
+    assert_equal(*result_pair.("a//aaa/a//a/aaa"))
+    assert_equal(*result_pair.("/aaa/a//a/aaa/a"))
+    assert_equal(*result_pair.("a//aaa/a//a/aaa/a"))
+    assert_equal(*result_pair.("a//aaa/a//a/aaa////"))
+    assert_equal(*result_pair.("a/a//aaa/a//a/aaa/a"))
+    assert_equal(*result_pair.("////a//aaa/a//a/aaa/a"))
+    assert_equal(*result_pair.("////a//aaa/a//a/aaa////"))
+    assert_equal(*result_pair.("."))
+    assert_equal(*result_pair.(".././"))
+    assert_equal(*result_pair.(".///.."))
+    assert_equal(*result_pair.("/././/"))
+    assert_equal(*result_pair.("//../././"))
+    assert_equal(*result_pair.(".///.../.."))
+    assert_equal(*result_pair.("/././/.//."))
+    assert_equal(*result_pair.("/...//../././"))
+    assert_equal(*result_pair.("/..///.../..//"))
+    assert_equal(*result_pair.("/./././/.//..."))
+    assert_equal(*result_pair.("/...//.././././/."))
+    assert_equal(*result_pair.("./../..///.../..//"))
+    assert_equal(*result_pair.("///././././/.//..."))
+    assert_equal(*result_pair.("./../..///.../..//././"))
+    assert_equal(*result_pair.("///././././/.//....///"))
+    assert_equal(*result_pair.("http://www.example.com"))
+    assert_equal(*result_pair.("foor for thought"))
+    assert_equal(*result_pair.("2gb63b@%TY25GHawefb3/g3qb"))
+  end
 end
