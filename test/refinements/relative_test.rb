af5a512dee8e7cfef7ca32425ac6127795f4203a
Daniel P. Clark <6ftdan@gmail.com>
2016-06-19 22:33:45 -0400

    Booleans unchanged on Pathname

--- test/refinements/relative_test.rb
+++ test/refinements/relative_test.rb
@@ -5,12 +5,17 @@ require "faster_path/optional/refinements"
 class RefinedPathname
   using FasterPath::RefinePathname
   def relative?(v)
-    Pathname.new(v).absolute?
+    Pathname.new(v).relative?
   end
 end 
 
 class RelativeRefinementTest < Minitest::Test
   def test_refines_pathname_relative?
-    assert RefinedPathname.new.relative?("/")
+    refute RefinedPathname.new.relative?("/")
   end 
+
+  def test_nil_behaves_the_same
+    assert_raises(TypeError) { RefinedPathname.new.relative?(nil) }
+    assert_raises(TypeError) { Pathname.new(nil).relative? }
+  end
 end
