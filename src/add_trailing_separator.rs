379e61dc4a4152b46534aabb54fbe47fb1e2452d
Daniel P. Clark <6ftdan@gmail.com>
2016-06-19 22:05:29 -0400

    add_trailing_separator safely takes nil

--- src/add_trailing_separator.rs
+++ src/add_trailing_separator.rs
@@ -4,25 +4,26 @@ use std::ffi::{CStr, CString};
 use std::str;
 
 #[no_mangle]
-pub extern "C" fn add_trailing_separator(string: *const c_char) -> *const c_char {
-    let c_str = unsafe {
-        assert!(!string.is_null());
-
-        CStr::from_ptr(string)
-    };
-    let r_str = str::from_utf8(c_str.to_bytes()).unwrap_or("");
-
-    if r_str.is_empty() {
-        return string;
+pub extern fn add_trailing_separator(string: *const c_char) -> *const c_char {
+  let c_str = unsafe {
+    if string.is_null() {
+      return string;
     }
+    CStr::from_ptr(string)
+  };
+  let r_str = str::from_utf8(c_str.to_bytes()).unwrap_or("");
 
-    let path = Path::new(r_str);
-    let out_str = if !(path.to_str().unwrap().chars().last().unwrap() == '/') {
-        format!("{}{}", path.to_str().unwrap(), MAIN_SEPARATOR)
-    } else {
-        path.to_str().unwrap().to_string()
-    };
+  if r_str.is_empty() {
+    return string;
+  }
 
-    let output = CString::new(out_str).unwrap();
-    output.into_raw()
+  let path = Path::new(r_str);
+  let out_str = if !(path.to_str().unwrap().chars().last().unwrap() == '/') {
+    format!("{}{}", path.to_str().unwrap(), MAIN_SEPARATOR)
+  } else {
+    path.to_str().unwrap().to_string()
+  };
+
+  let output = CString::new(out_str).unwrap();
+  output.into_raw()
 }
