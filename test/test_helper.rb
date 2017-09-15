a6028f4caa76064dac01dedbe211949320246850
Daniel P. Clark <6ftdan@gmail.com>
2017-09-15 09:43:19 -0400

    darn Rubocop namespace changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 if ENV['TEST_MONKEYPATCHES'] &&
     ENV['WITH_REGRESSION'] &&
+    !ENV['RUST_BACKTRACE'] &&
     ENV['TRAVIS_OS_NAME'] == 'linux' &&
     Gem::Dependency.new('', '~> 2.3', '< 2.4').match?('', RUBY_VERSION)
   require 'simplecov'
