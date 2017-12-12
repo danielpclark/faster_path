b48fea4ccd24155e15de9974f9e590a8e6040a5f
Dmitry Gritsay <unseductable@gmail.com>
2017-12-12 20:59:50 +0200

    Do not allocate Strings in extname
     - Use `&str` instead of `String` for `extname::extname`

--- src/extname.rs
+++ src/extname.rs
@@ -1,13 +1,13 @@
 use path_parsing::extract_last_path_segment;
 
-pub fn extname(pth: &str) -> String {
+pub fn extname(pth: &str) -> &str {
   let name = extract_last_path_segment(pth);
 
   if let Some(dot_i) = name.rfind('.') {
     if dot_i > 0 && dot_i < name.len() - 1 && name[..dot_i].chars().rev().next().unwrap() != '.' {
-      return String::from(&name[dot_i..])
+      return &name[dot_i..]
     }
   }
 
-  String::from("")
+  ""
 }
