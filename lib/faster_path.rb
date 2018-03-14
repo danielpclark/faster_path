e74349d3aee630bb38cd7ecbf8aabde9e2c905da
Daniel P. Clark <6ftdan@gmail.com>
2018-03-14 15:34:47 -0400

    Add YARD doc comments to Ruby code
    [ci skip]

--- lib/faster_path.rb
+++ lib/faster_path.rb
@@ -5,6 +5,9 @@ require 'faster_path/thermite_initialize'
 require 'fiddle'
 require 'fiddle/import'
 
+# FasterPath module behaves as a singleton object with the alternative method
+# implementations for Pathname, and some for File, available directly on it.
+#
 module FasterPath
   FFI_LIBRARY = begin
     toplevel_dir = File.dirname(__dir__)
@@ -23,30 +26,50 @@ module FasterPath
     private_class_method :children_compat
   end
 
+  # The Rust architecture bit width: 64bits or 32bits
+  # @return [Integer]
   def self.rust_arch_bits
     Rust.rust_arch_bits
   end
 
+  # The Ruby architecture bit width: 64bits or 32bits
+  # @return [Integer]
   def self.ruby_arch_bits
     1.size * 8
   end
 
+  # A very fast way to check if a string is blank
+  # @param str [#to_s]
+  # @return [true, false]
   def self.blank?(str)
     "#{str}".strip.empty?
   end
 
+  # Rust implementation of File.basename
+  # @param pth [String] the path to evaluate
+  # @param ext [String] any extension to remove
+  # @return [String]
   def self.basename(pth, ext="")
     Public.send(:basename, pth, ext)
   end
 
+  # Rust implementation of Pathname.children
+  # @param pth [String] the path to evaluate
+  # @param with_directory [true, false] whether to include directory in results
+  # @return [Array<String>]
   def self.children(pth, with_directory=true)
     Public.send(:children, pth, with_directory)
   end
 
+  # Rust implementation of Pathname.children
+  # @param pth [String] the path to evaluate
+  # @param with_directory [true, false] whether to include directory in results
+  # @return [Array<Pathname>]
   def self.children_compat(pth, with_directory=true)
     Public.send(:children_compat, pth, with_directory)
   end
 
+  # @private
   module Rust
     extend Fiddle::Importer
     dlload FFI_LIBRARY
--- lib/faster_path/thermite_initialize.rb
+++ lib/faster_path/thermite_initialize.rb
@@ -2,14 +2,19 @@ require 'rubygems'
 require 'rake/tasklib'
 require_relative './version'
 
+# @private :nodoc:
 module Thermite
+  # @private :nodoc:
   class Config
+    # @private :nodoc:
     def ruby_version
       "ruby#{RUBY_VERSION}"
     end
   end
 
+  # @private :nodoc:
   class Tasks < Rake::TaskLib
+    # @private :nodoc:
     def github_download_uri(_tag, version)
       "#{github_uri}/releases/download/v#{FasterPath::VERSION}/#{config.tarball_filename(version)}"
     end
